76eae5dad82334b352f150bb8c39edb068651bae
Gaia-System <melon000999@gmail.com>
2022-05-06 13:22:24 +0900

    Test Python 3, 5, 15 YearDream

--- yeardream2022/hello.py
+++ yeardream2022/hello.py
@@ -1,5 +1,10 @@
 print('hello world')
 
-for i in range(10):
-    if i % 3 == 0:
+for i in range(1, 21):
+    if i % 15 == 0:
+        print(f'{i} yeardream')
+    elif i % 3 == 0:
         print(f'{i} year')
+    elif i % 5 == 0:
+        print(f'{i} dream')
+    else: print(i)
